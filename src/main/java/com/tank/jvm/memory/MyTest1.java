8b301938d8dbea3d588924d584b1b4a2a038df1c
tank <347477499@qq.com>
2019-12-06 14:55:53 +0800

    JVM内存空间划分与讲解（未完待续）

---
+++ src/main/java/com/tank/jvm/memory/MyTest1.java
@@ -0,0 +1,19 @@
+package com.tank.jvm.memory;
+
+/**
+ * @author tank
+ * @create 2019/12/06 14:51
+ */
+/*
+    虚拟机栈：Stack Frame 栈帧
+    程序计数器：Programmer Counter
+    本地方法栈：主要处理本地方法
+    堆（heap）：JVM管理的最大一块内存空间
+    方法区（Method Area）：存储元信息。永久代（Permanent Generation），从jdk1.8开始，已经彻底废弃了永久代，
+    使用元空间（meta space）
+    运行时常量池：方法区的一部分内容
+    直接内存：Direct Memory
+ */
+public class MyTest1 {
+
+}
